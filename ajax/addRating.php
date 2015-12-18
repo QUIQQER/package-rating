4e4097896cd3edbd683932a6ff98595f555ca7ae
Henning Leutz <leutz@pcsg.de>
2015-12-18 12:43:23 +0100

    psr2

--- ajax/addRating.php
+++ ajax/addRating.php
@@ -7,25 +7,20 @@
  * @param String $siteId -
  * @param String $rating -
  *
- * @return Array
+ * @return array
  */
-function package_quiqqer_rating_ajax_addRating(
-    $project,
-    $siteId,
-    $rating
-) {
-    $Project = QUI::getProjectManager()->decode($project);
-    $Site    = $Project->get($siteId);
+QUI::$Ajax->registerFunction(
+    function ($project, $siteId, $rating) {
+        $Project = QUI::getProjectManager()->decode($project);
+        $Site    = $Project->get($siteId);
 
-    QUI\Rating\Handler::rate($Site, $rating);
+        QUI\Rating\Handler::rate($Site, $rating);
 
-    $result            = QUI\Rating\Handler::getRatingFromSite($Site);
-    $result['average'] = round($result['average'], 1);
+        $result            = QUI\Rating\Handler::getRatingFromSite($Site);
+        $result['average'] = round($result['average'], 1);
 
-    return $result;
-}
-
-QUI::$Ajax->register(
+        return $result;
+    },
     'package_quiqqer_rating_ajax_addRating',
     array('project', 'siteId', 'rating')
 );
